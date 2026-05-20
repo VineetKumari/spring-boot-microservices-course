f39142404994b2011b1483d722dfedeee04d575f
Vineet Kumari <vineetkumari1991@gmail.com>
2026-05-20 11:13:08 +0530

    currency exchange microservice changes

---
+++ microservices/currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,12 @@
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values
+(1001l,'USD','INR',65.00,null);
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values
+(1002l,'EUR','INR',75.00,null);
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values
+(1003l,'AUD','INR',25.00,null);
